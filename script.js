e32ba96fedd82db6d9b797cf917e0d664853cb76
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2024-01-24 00:19:10 -0600

    Create Book prototype

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const libraryDatabase = [];
 function addBookToLibrary(book) {
   libraryDatabase.push(book);
 }
+
+function Book(title, author, pages, wasRead = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.wasRead = wasRead;
+}
+
+Book.prototype.info = function() {
+  if (this.wasRead) {
+    return `${this.title} by ${this.author}, ${this.pages} pages, read.`
+  }
+  return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
+}
+
